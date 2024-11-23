03858e1332c410bb64e80dd006e50a984bb565b8
Yuriy <pye1984@gmail.com>
2024-11-23 19:28:37 +0700

    Решение задачи 2.1.3 Spring Beans

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,6 +2,8 @@ package app;
 
 import app.config.AppConfig;
 import app.model.AnimalsCage;
+import app.model.Dog;
+import app.model.Timer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -10,11 +12,11 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         for (int i = 0; i < 5; i++) {
             AnimalsCage bean =
                     applicationContext.getBean(AnimalsCage.class);
             bean.whatAnimalSay();
         }
     }
-
 }
